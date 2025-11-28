76537f7414ab5c6d4ac857c1e4bbcb5849e5721e
dzhakenir <dzhakenir@gmail.com>
2025-11-28 10:37:21 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,20 @@
-// реализация пользовательского приложения
-
+#include <arithmetic.h>
+#include <string>
+using namespace std;
 int main()
 {
-  return 0;
+	int flag = 1;
+	do {
+		cout << "write expression\nPossible operators: -a, a+b, a-b, a*b, a/b, sqrt(a), pow(a,b), ln(a), log2(a), log10(a), sin(a), cos(a), tan(a), asin(a), acos(a), atan(a)\nPossible constants: pi, e\nFunctions require brackets to work correctly\nDo not use spaces"<<endl;
+		string in;
+		cin >> in;
+		postfix expression(in);
+		double res = expression.calculate();
+		cout << res << "\nEnter number\n0 - exit\n1 - calculate new expression" << endl;
+		cin >> flag;
+		while (flag != 0 && flag != 1) {
+			cout << "wrong input";
+			cin >> flag;
+		}
+	} while (flag != 0);
 }
