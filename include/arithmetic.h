627b5b3a257e66872845a0fa94287c2e2c25d233
dzhakenir <dzhakenir@gmail.com>
2025-11-28 10:35:44 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,39 @@
-// объявление функций и классов для вычисления арифметических выражений
+
+#ifndef __POSTFIX_H__
+#define __POSTFIX_H__
+#include <stack.h>
+#include <string>
+
+using namespace std;
+
+enum TokenType {
+    EMPTY,
+    COMMA, LEFT_BRACKET, RIGHT_BRACKET,
+    VARIABLE, NUMBER, PI, E,
+    PLUS, MINUS, MUL, DIV, UNARY_MINUS, POW, 
+    SQRT, LN, LOG2, LOG10,
+    SIN, COS, TAN, ACOS, ASIN, ATAN
+};
+
+struct var {
+    string name;
+    bool given;
+    double val;
+    var(string a) :name(a), given(false) {};
+};//РЈ С‡РёСЃРµР» РЅРµС‚ РїСЂРёРѕСЂРёС‚РµС‚Р°, Сѓ С„СѓРЅРєС†РёР№ - Р·РЅР°С‡РµРЅРёСЏ
+
+struct token {
+    TokenType type;
+    double value;
+    int prior;
+    token(TokenType t = EMPTY, int pr = 0, double val = 0) noexcept :type(t), value(val), prior(pr) {}
+};
+
+class postfix {
+private:
+    stack<token> tokens;
+public:
+    postfix(const string& s);
+    double calculate();
+};
+#endif
