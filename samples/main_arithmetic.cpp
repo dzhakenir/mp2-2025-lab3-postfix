44789a8b25933c528e02699dbe4f67f40b0ed2c3
dzhakenir <dzhakenir@gmail.com>
2025-12-02 16:55:32 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -5,16 +5,18 @@ int main()
 {
 	int flag = 1;
 	do {
-		cout << "write expression\nPossible operators: -a, a+b, a-b, a*b, a/b, sqrt(a), pow(a,b), ln(a), log2(a), log10(a), sin(a), cos(a), tan(a), asin(a), acos(a), atan(a)\nPossible constants: pi, e\nFunctions require brackets to work correctly\nDo not use spaces"<<endl;
+		cout << "write expression\nPossible operators: -a, a+b, a-b, a*b, a/b, sqrt(a), ln(a), log2(a), log10(a), sin(a), cos(a), tan(a), asin(a), acos(a), atan(a)\nPossible constants: pi, e\nFunctions require brackets to work correctly\nDo not use spaces"<<endl;
 		string in;
 		cin >> in;
 		postfix expression(in);
-		double res = expression.calculate();
-		cout << res << "\nEnter number\n0 - exit\n1 - calculate new expression" << endl;
-		cin >> flag;
-		while (flag != 0 && flag != 1) {
-			cout << "wrong input";
+		do {
+			double res = expression.calculate();
+			cout << res << "\nEnter number\n0 - exit\n1 - calculate new expression\n2 - calculate this expression with new variable values" << endl;
 			cin >> flag;
-		}
+			while (flag != 0 && flag != 1 && flag != 2) {
+				cout << "wrong input";
+				cin >> flag;
+			}
+		} while (flag == 2);
 	} while (flag != 0);
 }
