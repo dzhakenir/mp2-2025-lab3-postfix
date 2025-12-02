0f561b18e7b63af8736526d3ff26103fb0b730e6
dzhakenir <dzhakenir@gmail.com>
2025-12-02 16:54:48 +0300

    Update test_arithmetic.cpp

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,3 +1,60 @@
-// тесты для вычисления арифметических выражений
+// С‚РµСЃС‚С‹ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+#define _USE_MATH_DEFINES
 
 #include <gtest.h>
+#include <arithmetic.h>
+double pi = M_PI;
+double e = M_E;
+TEST(postfix, test_1) {
+	EXPECT_EQ(postfix("1+2").calculate(),3);
+}
+TEST(postfix, test_2) {
+	EXPECT_EQ(postfix("3*5").calculate(), 15);
+}
+TEST(postfix, test_3) {
+	EXPECT_EQ(postfix("6/4").calculate(), 6.0/4);
+}
+TEST(postfix, test_4) {
+	EXPECT_EQ(postfix("sin(cos(2))").calculate(), sin(cos(2)));
+}
+TEST(postfix, test_5) {
+	EXPECT_EQ(postfix("x*y+z").calculate(2,3,4), 10);
+}
+TEST(postfix, test_6) {
+	EXPECT_EQ(postfix("sqrt(cos(1)*sin(2)-tan(pi*e))*10/33").calculate(), sqrt(cos(1) * sin(2) - tan(pi * e)) * 10 / 33);
+}
+TEST(postfix, test_7) {
+	EXPECT_EQ(postfix("3*4/5*6/7").calculate(), 3.0 * 4 / 5 * 6 / 7);
+}
+TEST(postfix, test_8) {
+	EXPECT_EQ(postfix("5/(6+7)*3*(3+(5+6)*4)/4+4").calculate(), 5.0 / (6 + 7) * 3 * (3 + (5 + 6) * 4) / 4 + 4);
+}
+TEST(postfix, test_9) {
+	EXPECT_EQ(postfix("sin(12*cos(4)/5+sqrt(123*pi/e)*32)/ln(43*29-98/log2(63)+498*log10(399998))").calculate(), sin(12 * cos(4) / 5 + sqrt(123 * pi / e) * 32) / log(43 * 29 - 98 / log2(63) + 498 * log10(399998)));
+}
+TEST(postfix, test_10) {
+	EXPECT_EQ(postfix("-3+(-4)*(-5)-6*(-3)/(-4)+(-6)").calculate(), -3.0 + (-4.0) * (-5) - 6.0 * (-3) / (-4) + (-6));
+}
+TEST(postfix, test_11) {
+	string s = "1";
+	for (int i = 0; i < 100000; i++)s += "+1";
+	EXPECT_EQ(postfix(s).calculate(), 1e5+1);
+}
+TEST(postfix, test_12) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
+TEST(postfix, test_13) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
+TEST(postfix, test_14) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
+TEST(postfix, test_15) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
+TEST(postfix, test_16) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
+TEST(postfix, test_17) {
+	EXPECT_EQ(postfix("1+2").calculate(), 3);
+}
